fe423a40b75501c0451e75d26e7ae827cea56417
Mikhail <elkinms85@gmail.com>
2025-02-13 00:51:58 +0200

    try to add hero name to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,26 @@
 import Navigation from "./Navigation.tsx";
 
+import {characters, defaultHero} from "../utils/constants.ts";
+import {useParams} from "react-router";
+import {useEffect, useState} from "react";
+
 const Header = () => {
+    const [hero, setHero] = useState<string | undefined>();
+    const {heroId = defaultHero} = useParams();
+
+    useEffect(() => {
+        if (characters[heroId]) {
+            setHero(characters[heroId].name);
+        }
+    }, []);
+
     return (
         <header className={'rounded-t-2xl bg-grey-color'}>
             <Navigation />
-            <h1 className="text-center text-3xl py-6">Luke Skywalker</h1>
+            <h1 className="text-center text-3xl py-6">{hero}</h1>
         </header>
     );
+
 };
 
 export default Header;
