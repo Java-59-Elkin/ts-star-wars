baf2d5b70ef2dba7746ac4cb1aa96087a0e555b5
Mikhail <elkinms85@gmail.com>
2025-02-16 23:39:11 +0200

    implement HOC to NavItem modules

--- src/components/hoc/heroWrapper.tsx
+++ src/components/hoc/heroWrapper.tsx
@@ -1,22 +1,24 @@
 import { useParams } from "react-router";
 import { characters, defaultHero } from "../../utils/constants.ts";
-import {FC, useContext, useEffect} from "react";
+import React, {useContext, useEffect} from "react";
 import {SWContext} from "../../utils/context.ts";
 import ErrorPage from "../ErrorPage.tsx";
 
-export const heroWrapper = (WrappedComponent: FC<never>) => props => {
+export const heroWrapper = (WrappedComponent: React.FunctionComponent) => {
+    return (props: any) => {
         const {heroId = defaultHero} = useParams();
         const {changeHero} = useContext(SWContext);
 
-    useEffect(() => {
-        if (!characters[heroId]) {
-            return
-        }
-        changeHero(heroId);
-    }, [heroId]);
+        useEffect(() => {
+            if (!characters[heroId]) {
+                return
+            }
+            changeHero(heroId);
+        }, [heroId]);
 
 
-    return characters[heroId] ? (
-        <WrappedComponent heroId={heroId} {...props} />
-    ) : <ErrorPage/>;
+        return characters[heroId] ? (
+            <WrappedComponent heroId={heroId} {...props} />
+        ) : <ErrorPage/>;
+    }
 }
